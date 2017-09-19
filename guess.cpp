a02c8762bc25dd3bdb13d47dbf11f502b489a30a
Bianca Cervantes A <31226206+KittyCervantes@users.noreply.github.com>
2017-09-18 18:32:45 -0700

    Create guess.cpp

---
+++ guess.cpp
@@ -0,0 +1,21 @@
+Author: Bianca Cervantes
+
+#include <iostream>
+
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+main()
+{
+srand (time(NULL));
+int randomNumber,gucci,diff;
+//gucci instead of guess, get it??...i need to stop.
+randomNumber=rand()%10+1
+
+cout<<"I'm thinking of a number between one and ten, can you guess it?""<<endl;
+cin>>gucci;
+
+if(gucci=randomNumber)
+
+}
