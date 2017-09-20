4c0028461472f8e8b1cf262ded1130b85359277a
KittyCervantes <bcervantes9@ecc-jaguar.csuchico.edu>
2017-09-20 11:18:17 -0700

    added if statements

--- guess.cpp
+++ guess.cpp
@@ -1,21 +1,40 @@
-Author: Bianca Cervantes
+//Author: Bianca Cervantes
 
 #include <iostream>
-
+#include <string>
 #include <cstdlib>
 #include <ctime>
 
 using namespace std;
+
 main()
 {
 srand (time(NULL));
 int randomNumber,gucci,diff;
-//gucci instead of guess, get it??...i need to stop.
-randomNumber=rand()%10+1
+//gucci instead of guess, get it??...I need to stop.
 
-cout<<"I'm thinking of a number between one and ten, can you guess it?""<<endl;
+randomNumber=rand()%10+1;
+diff=0;
+
+cout<<"I'm thinking of a number between one and ten, can you guess it?"<<endl;
 cin>>gucci;
 
-if(gucci=randomNumber)
+if(gucci = randomNumber) 
+{
+cout<<"Wow, you guessed it! You win this time!" <<endl;
+}
+else if(gucci > randomNumber)
+{
+diff == gucci-randomNumber;
+cout<<"Sorry, you were too high by "<<diff<<". I win!"<<endl;
+}
+else if(gucci < randomNumber)
+{
+diff == randomNumber-gucci;
+cout<<"Sorry, you were "<<diff<<" too low. I win!"<<endl;
+}
+
+
+return 0;
 
 }
