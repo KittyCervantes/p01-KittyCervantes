43b93f7bc355cbad7c3266c020004be1f6f7327c
KittyCervantes <bcervantes9@ecc-jaguar.csuchico.edu>
2017-09-20 13:20:49 -0700

    fixed if statements and order

--- guess.cpp
+++ guess.cpp
@@ -1,10 +1,10 @@
 //Author: Bianca Cervantes
 
-#include <iostream>
+#include<iostream>
+#include<string>
 
-
-#include <cstdlib>
-#include <ctime>
+#include<cstdlib>
+#include<ctime>
 
 using namespace std;
 
@@ -15,31 +15,33 @@ srand (time(NULL));
 int randomNumber,gucci,diff;
 //gucci instead of guess, get it??...I need to stop.
 
-randomNumber=rand()%10+1;
+randomNumber=rand() % 10+1;
 diff=0;
 
 cout<<"I'm thinking of a number between one and ten, can you guess it?"<<endl;
 cin>>gucci;
 
 if(gucci==randomNumber) 
-{
-cout<<"Wow, you guessed it! You win this time!" <<endl;
-}
-else if(gucci > randomNumber)
-{
-diff==gucci-randomNumber;
-cout<<"Sorry, you were too high by "<<diff<<". I win!"<<endl;
-}
-else if(gucci < randomNumber)
-{
-diff==randomNumber-gucci;
-cout<<"Sorry, you were "<<diff<<" too low. I win!"<<endl;
-}
+    {
+    cout<<"Wow, you guessed it! You win this time!" <<endl;
+    }
+else if(gucci > randomNumber && gucci < 10)
+    {
+    diff=gucci - randomNumber;
+    cout<<"Sorry, you were too high by "<<diff<<". I win!"<<endl;
+    }
+else if(gucci < randomNumber && gucci < 10)
+    {
+    diff=randomNumber - gucci;
+    cout<<"Sorry, you were too low by "<<diff<<". I win!"<<endl;
+    }
 
-else(gucci>10+1);
-{
-cout<<"Please only enter numbers between 1 and 10!"<<endl;
-}
+
+else if(gucci>=11)
+    {
+    cout<<"Please only enter numbers between 1 and 10!"<<endl;
+    }      
+
 
 
 return 0;
